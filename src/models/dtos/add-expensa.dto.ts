93d6bb7f1d9947d2ef49f4da406d01188dd62f54
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-09-27 17:01:50 -0400

    wip: dtos

---
+++ src/models/dtos/add-expensa.dto.ts
@@ -0,0 +1,24 @@
+import { ExpenseCategory } from '../enums/expense-category.enum';
+import { Status } from '../enums/status.enum';
+
+export class AddExpenseDto {
+  public readonly name: string;
+  public readonly category: ExpenseCategory;
+  public readonly description: string;
+  public readonly amount: number;
+  public readonly status: Status;
+
+  constructor(
+    name: string,
+    description: string,
+    amount: number,
+    category: ExpenseCategory,
+    status: Status
+  ) {
+    this.name = name;
+    this.category = category;
+    this.description = description;
+    this.amount = amount;
+    this.status = status;
+  }
+}
---
+++ src/models/dtos/delete-expense.dto.ts
@@ -0,0 +1,7 @@
+export class DeleteExpenseDto {
+  id: number;
+
+  constructor(id: number) {
+    this.id = id;
+  }
+}
